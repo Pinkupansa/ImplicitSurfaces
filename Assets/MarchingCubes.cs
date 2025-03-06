49f1c5f56448c07769d3b859dda261eb62243ea4
Sacha CERF <sacha@MacBook-Pro-de-Sacha-2.local>
2025-03-07 00:41:42 +0100

    made most parameters static field of the Marching Cubes class

--- Assets/MarchingCubes.cs
+++ Assets/MarchingCubes.cs
@@ -14,19 +14,31 @@ using UnityEngine;
 public static class MarchingCubes 
 {
     static float ISOPOTENTIAL = 0.1f;
-    static float EvaluatePoint(GridCoord coord, float gridStep, Vector3 basePoint, ImplicitSurfaceData surface, float[,,] gridPotentials, bool[,,] evaluatedPoints){
+
+    static float gridStep;
+    static int gridSize; 
+    static Vector3 basePoint; 
+    static ImplicitSurfaceData surface; 
+    static float[,,] gridPotentials; 
+    static bool[,,] evaluatedPoints;
+    static Dictionary<Edge, int> edgeIndices;
+    static List<Vector3> edgePositions;
+    static List<int> triangles;
+
+    
+    static float EvaluatePoint(GridCoord coord){
         if(!evaluatedPoints[coord.x, coord.y, coord.z]){
             gridPotentials[coord.x, coord.y, coord.z] = surface.EvaluatePot((Vector3)coord*gridStep + basePoint) - ISOPOTENTIAL;
             evaluatedPoints[coord.x, coord.y, coord.z] = true;
         }
         return gridPotentials[coord.x, coord.y, coord.z];
     }
-    static float[] EvaluateCube(GridCoord baseCornerCoord, float gridStep, Vector3 basePoint, ImplicitSurfaceData surface, float[,,] gridPotentials, bool[,,] evaluatedPoints){
+    static float[] EvaluateCube(GridCoord baseCornerCoord){
 
         float[] cornerPotentials = new float[8];
         for(int i = 0; i < 8; i++){
             GridCoord point = new GridCoord(baseCornerCoord.x + MCUtilities.vertexOffsets[i].x, baseCornerCoord.y + MCUtilities.vertexOffsets[i].y, baseCornerCoord.z + MCUtilities.vertexOffsets[i].z);
-            cornerPotentials[i] = EvaluatePoint(point, gridStep, basePoint, surface, gridPotentials, evaluatedPoints);
+            cornerPotentials[i] = EvaluatePoint(point);
         }
         return cornerPotentials;
     }
@@ -42,67 +54,76 @@ public static class MarchingCubes
         return cubeCase;
     }
 
-    public static Mesh MarchingCubesCPU(ImplicitSurfaceData surface, Vector3 centerPoint){
+    static Vector3 InterpolateMeshVertexPosition(GridCoord gc1, GridCoord gc2){
 
-		float gridStep = surface.gridStep;
-		int gridSize = surface.gridSize;
+        //evaluate potentials (if not already done) and interpolate border placement
+        float pot1 = EvaluatePoint(gc1);
+        float pot2 = EvaluatePoint(gc2);
+        return Vector3.Lerp(gc1, gc2, Mathf.Abs(pot1/(pot1-pot2))) * gridStep + basePoint;
+    }
+
+    static void AddTriangles(int[] localTriangles, GridCoord currentCoord){
+
+        for(int i = localTriangles.Length - 2; i >= 0; i--){ 
+            
+            //calculate the global coords of the edge extremities 
+            int[] edgeExtLocalIndices = MCUtilities.edgeTable[localTriangles[i]];
+            GridCoord ext1GridCoord = currentCoord + MCUtilities.vertexOffsets[edgeExtLocalIndices[0]];
+            GridCoord ext2GridCoord = currentCoord + MCUtilities.vertexOffsets[edgeExtLocalIndices[1]];
+            Edge e = new Edge(ext1GridCoord, ext2GridCoord);
+
+            if(!edgeIndices.ContainsKey(e)){
+                //add this edge as a vertex
+                edgeIndices.Add(e, edgeIndices.Count);
+                edgePositions.Add(InterpolateMeshVertexPosition(ext1GridCoord, ext2GridCoord));
+            }
+
+            //add the next vertex index of the triangle array
+            triangles.Add(edgeIndices[e]); 
+        }
+    }
+    public static Mesh MarchingCubesCPU(ImplicitSurfaceData _surface, Vector3 centerPoint){
+        surface = _surface;
+        gridStep = surface.gridStep;
+        gridSize = surface.gridSize;
 
         Mesh mesh = new Mesh();
 
         //vertices of the mesh will be on edges
 
         //table to keep track of the global edge indices in the mesh vertex array
-        Dictionary<Edge, int> edgeIndices = new Dictionary<Edge, int>(); 
+        edgeIndices = new Dictionary<Edge, int>(); 
         
         //positions of the center of edges which will serve as vertices
-        List<Vector3> edgesPositions = new List<Vector3>();
+        edgePositions = new List<Vector3>();
 
-        float[,,] gridPotentials = new float[gridSize, gridSize, gridSize];
-        bool[,,] evaluatedPoints = new bool[gridSize, gridSize, gridSize];
+        gridPotentials = new float[gridSize, gridSize, gridSize];
+        evaluatedPoints = new bool[gridSize, gridSize, gridSize];
         //a list of edge indices to read in triplets to obtain the mesh triangles
-        List<int> triangles = new List<int>();
-        Vector3 basePoint = centerPoint - gridSize/2f * gridStep * Vector3.one;
+        triangles = new List<int>();
+        basePoint = centerPoint - gridSize/2f * gridStep * Vector3.one;
 
         for(int x = 0; x < gridSize-1; x++){ //stop iterations at -1 because EvaluateCube looks at +1
             for(int y = 0; y < gridSize-1; y++){ 
                 for(int z = 0; z < gridSize-1; z++){
                 
                     GridCoord currentCoord = new GridCoord(x, y ,z); 
-					int cubeCase = IdentifyCubeCase(EvaluateCube(currentCoord, gridStep, basePoint, surface, gridPotentials, evaluatedPoints));
+                    int cubeCase = IdentifyCubeCase(EvaluateCube(currentCoord));
                     //the local indices of the edges forming the triangles
                     int[] localTriangles = MCUtilities.triTable[cubeCase];
 
-                    for(int i = localTriangles.Length - 2; i >= 0; i--){ 
-                        
-                        //calculate the global coords of the edge extremities 
-                        int[] edgeExtLocalIndices = MCUtilities.edgeTable[localTriangles[i]];
-                        GridCoord ext1GridCoord = currentCoord + MCUtilities.vertexOffsets[edgeExtLocalIndices[0]];
-                        GridCoord ext2GridCoord = currentCoord + MCUtilities.vertexOffsets[edgeExtLocalIndices[1]];
-                        Edge e = new Edge(ext1GridCoord, ext2GridCoord);
-
-                        if(!edgeIndices.ContainsKey(e)){
-                            //add this edge as a vertex
-                            edgeIndices.Add(e, edgeIndices.Count);
-                            float pot1 = EvaluatePoint(ext1GridCoord, gridStep, basePoint, surface, gridPotentials, evaluatedPoints);
-                            float pot2 = EvaluatePoint(ext2GridCoord, gridStep, basePoint, surface, gridPotentials, evaluatedPoints);
-                            edgesPositions.Add(Vector3.Lerp(ext1GridCoord, ext2GridCoord, Mathf.Abs(pot1/(pot1-pot2))) * gridStep + basePoint);
-                        }
-
-                        //add the next vertex index of the triangle array
-                        triangles.Add(edgeIndices[e]); 
-                    }
-
-			    } 
+                    AddTriangles(localTriangles, currentCoord);
+                }
             } 
         }
 
-        mesh.vertices = edgesPositions.ToArray();
-        mesh.triangles = triangles.ToArray();
-        mesh.RecalculateNormals(); 
+            mesh.vertices = edgePositions.ToArray();
+            mesh.triangles = triangles.ToArray();
+            mesh.RecalculateNormals(); 
 
-        return mesh; 
+            return mesh; 
+        }
     }
-}
 
 public class Edge{
     GridCoord u;
